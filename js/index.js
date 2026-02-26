88a9a05ede4ac4c79a61d176ef9825c89f6fb14a
ALAMIN761740 <abdullahalamin761740@gmail.com>
2026-02-27 00:27:39 +0600

    Basic Calculator work completed

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const buttonsE1 = document.querySelectorAll("button");
+
+const inputFieldE1 = document.getElementById("result-display");
+
+
+
+for (let i = 0; i < buttonsE1.length; i++) {
+    buttonsE1[i].addEventListener("click", () => {
+        const buttonValue = buttonsE1[i].textContent;
+        if (buttonValue === "C") {
+            clearResult();
+        }
+        else if (buttonValue === "=") {
+            calculateResult();
+        }   
+        else {
+            appendToResult(buttonValue);
+        }
+    });
+}
+    
+
+
+
+
+function clearResult() {
+    inputFieldE1.value = "";
+}
+
+
+function calculateResult() {
+    inputFieldE1.value = eval(inputFieldE1.value);
+}
+
+
+function appendToResult(buttonValue) {
+    inputFieldE1.value += buttonValue;
+}
